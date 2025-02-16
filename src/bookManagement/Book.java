d6f5aaf26f4a60cbc47e7f1da6066a619d48e71a
shubha-shree-r <shubhashree1805@gmail.com>
2025-02-16 14:43:17 +0530

    modified isuniqueisbn method

--- src/bookManagement/Book.java
+++ src/bookManagement/Book.java
@@ -23,88 +23,33 @@ public class Book {
     }
 
 
-    // Getters
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
     public String getISBN() {
         return ISBN;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getPublicationDate() {
-        return publicationDate;
-    }
-
-    public int getNumberOfCopies() {
-        return numberOfCopies;
-    }
-
-    // Setters
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public void setPublicationDate(String publicationDate) {
-        this.publicationDate = publicationDate;
-    }
-
-    public void setNumberOfCopies(int numberOfCopies) {
-        this.numberOfCopies = numberOfCopies;
-    }
-
-    // Display Book Details
-    public void displayBookInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("ISBN: " + ISBN);
-        System.out.println("Genre: " + genre);
-        System.out.println("Publication Date: " + publicationDate);
-        System.out.println("Number of Copies: " + numberOfCopies);
-    }
-
-
-    public boolean isUniqueISBN(String isbn,String ISBN) {
-        return !(isbnSet.contains(isbn) && isbnSet.contains(ISBN));
-    }
-
-
-    public boolean addBook(String isbn,String ISBN) {
-        if (isUniqueISBN(isbn,ISBN)) {
-            isbnSet.add(isbn);
-            isbnSet.add(ISBN);
-            return true;
+    public boolean isUniqueISBN(String isbn1) {
+        if(getISBN() != isbn1){
+            return !isbnSet.contains(isbn1);
         }
         return false;
+    }
+
+    public boolean addBook(String isbn1) {
+        if (isUniqueISBN(isbn1)) {
+            isbnSet.add(isbn1);
+
+            return true; // Successfully added
+        }
+
+        return false; // At least one ISBN already exists
     }
 
     public static void main(String[] args) {
         Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565",
                 "Classic", "1925-04-10", 10);
-        book.displayBookInfo();
-        System.out.println(book.addBook("978-0743273565",book.getISBN()));
-        System.out.println(book.addBook("978-0743273569",book.getISBN()));
-        System.out.println(book.addBook("978-0743273565",book.getISBN()));
+        System.out.println(book.addBook("978-0743273565"));
+        System.out.println(book.addBook("978-0743273569"));
+        System.out.println(book.addBook("978-0743273565"));
     }
 }
 
